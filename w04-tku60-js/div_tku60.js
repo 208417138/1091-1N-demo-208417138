9180d2ff6e5ef2b73990d342c60afdefab794e6c
208417138 <72386132+208417138@users.noreply.github.com>
2020-10-05 21:25:18 +0800

    Update div_tku60.js

--- w04-tku60-js/div_tku60.js
+++ w04-tku60-js/div_tku60.js
@@ -16,7 +16,7 @@ function changeVideo(index) {
     case 4:
       p.innerHTML =
         '<iframe width="560" height="315" src="https://www.youtube.com/embed/ZyDbq-lEKTo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-      break ;
+      break;
     case 5:
       p.innerHTML =
         '<iframe width="560" height="315" src="https://www.youtube.com/embed/NlsrJbVvjaA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
